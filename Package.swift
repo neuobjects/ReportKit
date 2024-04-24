f0c80082c4bddd0143049fac78d154ceac0e0c2f
Brian Lazarz <blazarz@neuobjects.com>
2024-04-24 08:49:13 -0500

    Updated version to 1.0.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
       //.binaryTarget(name: "ReportKit", path: "./Sources/ReportKit.xcframework")
-      .binaryTarget(name: "ReportKit", url: "https://localhost/Frameworks/ReportKit.xcframework.zip", checksum: "f669a4522ed2b6feeb483db9751fdedc326989e9edeca27a21bfd93bcd40307f")
+      .binaryTarget(name: "ReportKit", url: "https://localhost/Frameworks/ReportKit.xcframework.zip", checksum: "5f3ec40a27bf624567f7c7d0051bfa8f0d84a807ef48c0ad0faa059a4c4abfce")
     ]
     
     //LOOK AT DGCHARTSPACKAGE. I THINK WE'RE MESSING UP THE TARGET. IS IT REALLY A TARGET (THE OUTPUT)? WHAT IS THIS REALLY BUILDING? IS THIS SUPPLEMENTAL TO THE XCFRAMEWORK MENTIONED HERE - DO WE NEED TO DISTRIBUTE THEM TOGETHER SOMEHOW?
